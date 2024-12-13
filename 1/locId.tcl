a8d4dd6c5ce8796e181031a0922d20eca1861488
Oscar Sjoberg <oscarbror.sjoberg@gmail.com>
2024-12-13 17:02:25 +0100

    day one task 2

--- 1/locId.tcl
+++ 1/locId.tcl
@@ -1,9 +1,30 @@
 #!/bin/tclsh
 
-set infile [open "./input" r]
-set numberLines 0
 
-set totSum 0
+proc lcount_sorted {inputList value} {
+    # assumes sorted list
+    set occurence 0
+    set index [lsearch $inputList $value]
+    if { $index < 0 } {
+        return $occurence
+    }
+    incr occurence
+    set nextValue [ lindex $inputList [expr $index + $occurence]]
+    while { $nextValue == $value } {
+        incr occurence
+        set nextValue [ lindex $inputList [expr $index + $occurence]]
+    }
+    return $occurence
+}
+
+if {$argc != 1 } {
+    puts "Select input file"
+}
+
+set input_file [lindex $argv 0]
+
+set infile [open $input_file r]
+set numberLines 0
 
 set Llist {}
 set Rlist {}
@@ -11,24 +32,42 @@ set Rlist {}
 while { [gets $infile line] >=  0} {
 
         set lists [split $line " "]
-        puts $lists
 
         set Llist [concat $Llist [lindex $lists 0 ]]
-
         set Rlist [concat $Rlist [lindex $lists 1 ]]
 
-
-        #set sllist [ lsort $llist]
-        #set srlist [ lsort $rlist]
-        #
-        #
         incr numberLines
-
 }
+
+close $infile
 
 set sLlist [lsort $Llist]
 set sRlist [lsort $Rlist]
 
+set simScore 0
+
+set leftindex 0
+set rightindex 0
+
+# task two
+while { [llength $sLlist] > $leftindex } {
+    set value [lindex $sLlist $leftindex]
+    set lOcc [lcount_sorted $sLlist $value]
+    set rOcc [lcount_sorted $sRlist $value]
+    set leftindex [ expr $leftindex + $lOcc ]
+
+    #puts "value $value"
+    #puts "lOcc $lOcc"
+    #puts "rOcc $rOcc"
+    #puts "leftindex $leftindex"
+
+    set mulA [expr $rOcc * $value]
+    set simScore [expr $simScore + [ expr $lOcc * $mulA ]]
+    #puts "simScore $simScore"
+
+}
+
+# task one
 set listSum 0
 
 for {set i 0} {$i < [ llength $sLlist] } {incr i} {
@@ -38,14 +77,11 @@ for {set i 0} {$i < [ llength $sLlist] } {incr i} {
 
     set diff [expr $a - $b]
     set adiff [expr abs($diff)]
-    puts "adiff $adiff"
     set listSum [expr $listSum + $adiff]
-    puts "listSum $listSum"
 }
 
-puts "list length: [llength $Rlist]"
 
-close $infile
-puts "Tot: $totSum"
+
 puts "Number of lines: $numberLines"
 puts "listSum: $listSum"
+puts "simScore fin $simScore"
